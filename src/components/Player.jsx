6a31d8eb7135efd1ee11d3ae47f203021123f0a1
rituraj1511 <rituraj6@deloitte.com>
2023-11-07 21:41:11 +0530

    First Commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" name="" id="" value={name} />;
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
